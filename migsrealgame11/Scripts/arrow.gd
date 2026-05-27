e002e20c39dae121f853f4846bfbb794cc478232
migsob32-kings <migsob32@kingshigh.school.nz>
2026-05-27 13:21:52 +1200

    v3
    arrow stuff and anims

--- migsrealgame11/Scripts/arrow.gd
+++ migsrealgame11/Scripts/arrow.gd
@@ -1,21 +1,18 @@
 extends RigidBody2D
 
 func _ready():
-	# Enable gravity for the arrow
 	gravity_scale = 1.0
-	
-	# Optional: Make arrow stick to objects on collision
+	scale = Vector2(2.0, 2.0)
 	contact_monitor = true
 	max_contacts_reported = 1
 
 func _physics_process(delta):
-	# Rotate arrow to match its velocity direction
+	scale = Vector2(2.0, 2.0)
+	
 	if linear_velocity.length() > 10:
 		rotation = linear_velocity.angle()
 
 func _on_body_entered(body):
-	# Make arrow stick when it hits something
 	freeze = true
-	# Optional: queue_free after a few seconds
 	await get_tree().create_timer(3.0).timeout
 	queue_free()
--- migsrealgame11/Scripts/player.gd
+++ migsrealgame11/Scripts/player.gd
@@ -5,8 +5,10 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Shooting constants
-const SHOOT_POWER = 800.0
+const SHOOT_POWER = 1200
 const TRAJECTORY_POINTS = 30
+const BOW_OFFSET_X = 38
+const BOW_OFFSET_Y = -47
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -19,15 +21,16 @@ var jump_buffer_timer = 0.0
 # Animation
 var was_on_floor_last_frame = false
 var landing_tween: Tween
+var is_jumping = false
 
 # Shooting
 var is_aiming = false
 var trajectory_line: Line2D
+var is_firing = false
 
 @export var arrow_scene: PackedScene
 
 func _ready():
-	# Create trajectory line
 	trajectory_line = Line2D.new()
 	trajectory_line.width = 3
 	trajectory_line.default_color = Color.RED
@@ -37,19 +40,19 @@ func _ready():
 func _physics_process(delta):
 	var just_landed = is_on_floor() and not was_on_floor_last_frame and velocity.y > 0
 	
-	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		coyote_timer -= delta
+		is_jumping = true
 	else:
 		coyote_timer = COYOTE_TIME
+		is_jumping = false
 	
 	if just_landed:
 		play_landing_squash()
 	
 	was_on_floor_last_frame = is_on_floor()
 	
-	# Jump
 	if Input.is_action_just_pressed("ui_accept"):
 		jump_buffer_timer = JUMP_BUFFER_TIME
 	
@@ -60,37 +63,63 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 		jump_buffer_timer = 0.0
 		coyote_timer = 0.0
+		is_jumping = true
+		play_animation("jump")
 	
-	# Movement
 	var direction = Input.get_axis("left", "right")
 	if direction != 0:
 		velocity.x = direction * SPEED
 		flip_sprite(direction)
+		if not is_aiming and not is_firing and not is_jumping:
+			play_animation("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED * delta * 8)
+		if not is_aiming and not is_firing and not is_jumping:
+			play_animation("idle")
 	
-	# Shooting
 	handle_shooting(delta)
 	
 	move_and_slide()
 
 func handle_shooting(delta):
-	# Show trajectory while aiming
-	if Input.is_action_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot"):
 		is_aiming = true
+		is_firing = true
 		update_trajectory()
 		trajectory_line.show()
+		play_animation("beginfire")
 	
-	# Shoot instantly on release
+	if Input.is_action_pressed("shoot") and is_aiming:
+		var sprite = get_sprite_node()
+		if sprite and sprite.animation != "holdfire":
+			play_animation("holdfire")
+		update_trajectory()
+	
 	if Input.is_action_just_released("shoot") and is_aiming:
+		play_animation("endfire")
+		await get_tree().create_timer(0.3).timeout
 		shoot_arrow()
 		is_aiming = false
+		is_firing = false
 		trajectory_line.hide()
+		var direction = Input.get_axis("left", "right")
+		if direction != 0:
+			play_animation("walk")
+		else:
+			play_animation("idle")
+
+func get_bow_position() -> Vector2:
+	var offset_x = BOW_OFFSET_X
+	var sprite = get_sprite_node()
+	if sprite and sprite.flip_h:
+		offset_x = -BOW_OFFSET_X + 18  # Changed from -10 to +5
+	return global_position + Vector2(offset_x, BOW_OFFSET_Y)
 
 func update_trajectory():
 	var mouse_pos = get_global_mouse_position()
-	var start_pos = global_position
-	var direction = (mouse_pos - start_pos).normalized()
+	var bow_pos = get_bow_position()
+	
+	var direction = (mouse_pos - bow_pos).normalized()
 	var velocity_vector = direction * SHOOT_POWER
 	
 	var points = []
@@ -98,8 +127,8 @@ func update_trajectory():
 	
 	for i in range(TRAJECTORY_POINTS):
 		var time = i * time_step
-		var x = start_pos.x + velocity_vector.x * time
-		var y = start_pos.y + velocity_vector.y * time + 0.5 * gravity * time * time
+		var x = bow_pos.x + velocity_vector.x * time
+		var y = bow_pos.y + velocity_vector.y * time + 0.5 * gravity * time * time
 		var local_point = to_local(Vector2(x, y))
 		points.append(local_point)
 	
@@ -107,19 +136,22 @@ func update_trajectory():
 
 func shoot_arrow():
 	if arrow_scene == null:
-		print("ERROR: Arrow scene not set! Drag arrow.tscn to Inspector")
+		print("ERROR: Arrow scene not set!")
 		return
 	
 	var arrow = arrow_scene.instantiate()
 	get_parent().add_child(arrow)
-	arrow.global_position = global_position
+	
+	var bow_pos = get_bow_position()
+	arrow.global_position = bow_pos
 	
 	var mouse_pos = get_global_mouse_position()
-	var direction = (mouse_pos - global_position).normalized()
+	var direction = (mouse_pos - bow_pos).normalized()
 	var velocity_vector = direction * SHOOT_POWER
 	
 	arrow.linear_velocity = velocity_vector
 	arrow.rotation = direction.angle()
+	arrow.add_collision_exception_with(self)
 
 func play_landing_squash():
 	if landing_tween:
@@ -143,6 +175,12 @@ func flip_sprite(direction: float):
 			sprite.flip_h = false
 		elif direction < 0:
 			sprite.flip_h = true
+
+func play_animation(anim_name: String):
+	var sprite = get_sprite_node()
+	if sprite and sprite is AnimatedSprite2D:
+		if sprite.animation != anim_name:
+			sprite.play(anim_name)
 
 func get_sprite_node():
 	if has_node("Sprite2D"):
