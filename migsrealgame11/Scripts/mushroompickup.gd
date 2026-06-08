462400c3f125e0e07b8b745b02d03a6fbec1f5e3
migsob32-kings <migsob32@kingshigh.school.nz>
2026-06-08 14:55:05 +1200

    v5.6
    shroom pickup reworked

--- migsrealgame11/Scripts/mushroompickup.gd
+++ migsrealgame11/Scripts/mushroompickup.gd
@@ -1,19 +1,5 @@
 extends Area2D
 
-@export var pickup_type = "mushroom"
-@export var pickup_amount = 1
-
-func _ready():
-	body_entered.connect(_on_body_entered)
-
 func _on_body_entered(body):
-	print("Collected: ", pickup_type)
-	
-	# Check if it's the player
-	if body.name == "Player" or body.is_in_group("player"):
-		# Add to player inventory
-		if body.has_method("add_to_inventory"):
-			body.add_to_inventory(pickup_type, pickup_amount)
-		
-		# Delete the pickup
-		queue_free()
+	print("+1 Shroom")
+	queue_free()
